aab347578532e8583354a91405ffd1e0c80ec5d0
1286438215@qq.com <1286438215@qq.com>
2024-01-29 15:13:21 +0800

    vb

---
+++ scripts/shared.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+
+export const rootPath = path.resolve(__dirname, '../')
+export const iconsPath = path.resolve(rootPath, 'icons')
+export const sfcPath = path.resolve(rootPath, 'packages')
+export const distPath = path.resolve(rootPath, 'dist')
+
+/**
+ * Get the PascalCased name from a kebab-cased filename
+ * @param name file
+ */
+export const toPascalCase = (name: string): string => {
+  return name.split('-').map(e => e[0].toUpperCase() + e.slice(1)).join('')
+}
